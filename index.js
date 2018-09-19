065225ba7eae21ddadb5cba8343d5c31e9096147
Vong3432 <36991424+Vong3432@users.noreply.github.com>
2018-09-20 00:55:20 +0800

    menu

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ $(document).ready(function(){
 	$(".header-text").hide();
 	$("#ourCoffee").hide();
 	$("#coffeeContent").hide();
+	$("#food").hide();
+	$("#drink").hide();
+	$("#sample1").hide();
+	$("#sample2").hide();
+	$("#source1").hide();
+	$("#source2").hide();
 });
 
 $(document).on('scroll', function() {
@@ -20,9 +26,29 @@ $(document).on('scroll', function() {
 			
 			$("#coffeeContent").show();
 	        $("#coffeeContent").addClass('animated fadeInLeft slower delay-4s');
-    	}
+
+	        if( $(this).scrollTop() > $('#hitpoint').position().top )
+	    	{
+	    		$("#source1").show();
+	    		$("#drink").show(3000);
+		        $("#drink").addClass('animated fadeInLeft delay-4s');
+
+		        $("#sample1").show("fast");
+		        $("#sample1").addClass('animated fadeIn slow');
+
+		        if( $(this).scrollTop() > $('#sample1').position().top )
+	    		{
+	    			$("#source2").show();
+	    			$("#food").show(3000);
+		        	$("#food").addClass('animated fadeInLeft delay-4s');
+
+	    			$("#sample2").show();
+	    			$("#sample2").addClass('animated fadeIn slow');
+	    		}
+	    	}
+
+    	}    	
 
     }
-
 
 });
